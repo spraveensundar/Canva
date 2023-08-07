1ac235b822b5d7961ce3c8549b671d4fb10a523a
Praveen sundar <131776093+spraveensundar@users.noreply.github.com>
2023-08-07 19:28:39 +0530

    Add files via upload

---
+++ Landing page two/js/app.js
@@ -0,0 +1,51 @@
+
+
+
+
+$('#slide-one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+	dots:false,
+	autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+	autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+
+
+$(".trigger").click(function(){
+	
+	$(".navbar-collapse").collapse("hide")
+})
